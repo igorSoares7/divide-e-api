16bae34fc2bc819ecc26c09f914daaa6ccab50e3
igorSoares7 <igorsoaresr_contato@hotmail.com>
2024-09-28 13:49:08 -0300

    Cria a primeira versão do layout e adiciona a função somaNumeros

---
+++ script.js
@@ -0,0 +1,12 @@
+function somarNumeros(){
+   const numero = document.getElementById("resultadoSoma");
+   const numeroUm = document.getElementById("formUm").value;
+   const numeroDois = document.getElementById("formDois").value;
+   const operacao = Number(numeroUm) + Number(numeroDois)
+   numero.innerHTML = operacao
+   
+}
+
+function buscarPokemon(){
+    
+}
