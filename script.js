29b3404cf41d9c6d2631020a8913d3175283e4f0
igorSoares7 <igorsoaresr_contato@hotmail.com>
2024-09-28 14:46:24 -0300

    Adiciona as funções para a chamada da PokemonAPI

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 function somarNumeros(){
-   const numero = document.getElementById("resultadoSoma");
-   const numeroUm = document.getElementById("formUm").value;
-   const numeroDois = document.getElementById("formDois").value;
-   const operacao = Number(numeroUm) + Number(numeroDois)
-   numero.innerHTML = operacao
+   const numero = document.getElementById('resultadoSoma');
+   const numeroUm = document.getElementById('formUm').value;
+   const numeroDois = document.getElementById('formDois').value;
+   const operacao = Number(numeroUm) + Number(numeroDois);
+   numero.innerHTML = operacao;
    
 }
 
-function buscarPokemon(){
-    
+async function buscarPokemon(){
+   const valueInput = document.getElementById('formPoke').value;
+
+   const pokemon = await buscarAPI(valueInput);
+
+   const nomePoke = document.getElementById('nomePoke');
+   const imgPoke = document.getElementById('imgPoke');
+
+   nomePoke.innerHTML = `Nome: ${pokemon.name}`
+   imgPoke.src = pokemon.sprites.front_default
+}
+
+async function buscarAPI(valueInput){
+   const url = `https://pokeapi.co/api/v2/pokemon/${valueInput}/`
+
+   const response = await fetch(url)
+   return response.json()
+
+
 }
